7f54898e35896e88c5378cf16b99dfa48ba3e031
sync <85231933+syncwrld@users.noreply.github.com>
2024-02-01 23:00:12 -0300

    Removed info log

--- src/main/java/org/reflections/Reflections.java
+++ src/main/java/org/reflections/Reflections.java
@@ -337,10 +337,6 @@ public class Reflections implements NameHelper {
         keys += map.size();
         values += map.values().stream().mapToLong(Set::size).sum();
       }
-      log.info(
-          format(
-              "Reflections took %d ms to scan %d urls, producing %d keys and %d values",
-              System.currentTimeMillis() - start, urls.size(), keys, values));
     }
     return storeMap;
   }
